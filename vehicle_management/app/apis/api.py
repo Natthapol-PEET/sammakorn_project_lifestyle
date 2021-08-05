f36681403b9f1799ea567c16f4dc0e3225a90c18
Natthapol-PEET <natthapol.n@ku.th>
2021-08-05 13:40:25 +0700

    delete

--- vehicle_management/app/apis/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-api_app = FastAPI()
-
-
-@api_app.get('/')
-def test():
-    return {'message': 'unprotected api_app endpoint'}
--- vehicle_management/init_database/resident_home.sql
+++
@@ -1,9 +0,0 @@
-
-
-
-INSERT INTO resident_home (resident_id, home_id, create_datetime) VALUES 
-    (1, 1, NOW()),
-    (1, 4, NOW())
-
-
-SELECT * FROM resident_home;
